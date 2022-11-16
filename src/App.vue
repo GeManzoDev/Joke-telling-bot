e5125677c7b582ce92249441b0c3c821e2225aba
Gennaro Manzo <gennaromanzo1993@gmail.com>
2022-11-16 12:21:14 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ListenJoke></ListenJoke>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListenJoke from './components/Listen-joke.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ListenJoke },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: 'Architects Daughter', cursive;
+  background-color: #f8f6f4;
 }
 </style>
---
+++ src/components/Listen-joke.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <img class="funny-kid" src="../assets/Standing.png" alt="" />
+    <div class="bubble">
+      <img class="bubble-cloud" src="../assets/v999-14.jpg" alt="" />
+      <div class="joke">
+        Lorem ipsum dolor sit amet lorem Lorem ipsum dolor sit amet consectetur,
+      </div>
+    </div>
+    <h3>Hey wanna hear a joke?</h3>
+    <button class="btn">Hear a joke!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Listen-joke',
+};
+</script>
+
+<style scooped>
+.container {
+  top: 30px;
+  width: 50vw;
+  height: auto;
+  text-align: center;
+  position: relative;
+  margin: auto;
+}
+.container h3 {
+  font-size: 30px;
+}
+
+.funny-kid {
+  text-align: center;
+  margin: auto;
+}
+.bubble {
+  position: absolute;
+  display: flex;
+  top: -58px;
+  left: 450px;
+  width: 250px;
+  height: auto;
+}
+.bubble-cloud {
+  width: 100%;
+  height: auto;
+}
+.joke {
+  font-size: 10px;
+  position: relative;
+  left: -220px;
+  top: 90px;
+  background-color: transparent;
+  min-width: 70%;
+  padding: 5px;
+}
+.btn {
+  border-radius: 6px;
+  padding: 3px 5px;
+  font-size: 18px;
+  cursor: pointer;
+  background-image: radial-gradient(
+      circle at 49.45% 10.73%,
+      #ff8008,
+      transparent 100%
+    ),
+    radial-gradient(circle at 87.88% 94.1%, #ffc837, transparent 100%),
+    radial-gradient(circle at 50% 50%, #ffffff, #ffffff 100%);
+}
+.btn:hover {
+  transform: scale(0.97);
+}
+</style>
